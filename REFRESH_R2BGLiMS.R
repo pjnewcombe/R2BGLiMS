ea1159aab6b10ff6b4549b85701b455caf1cf523
pjnewcombe <pjnewcombe@gmail.com>
2014-12-16 18:25:33 +0000

    Adding extra files

---
+++ REFRESH_R2BGLiMS.R
@@ -0,0 +1,20 @@
+package.name <- 'R2BGLiMS'
+package.location <- '/Users/pauln/Dropbox/Work Projects/R Packages'
+github.username <-'pjnewcombe'
+commit.message <- NULL
+#commit.message <- "Unnessary specification of N for the marginal model removed."
+
+### --- Java code and arguments file
+system("rm -rf inst/BGLiMS/*")
+system( paste("cp -rf '/Users/pauln/Dropbox/Work Projects/RJ Java Package/Java Code/BGLiMS/dist/'* inst/BGLiMS/", sep="") )
+system( paste("cp '/Users/pauln/Dropbox/Work Projects/RJ Java Package/Test/Arguments/Arguments_Package.txt' inst/BGLiMS/", sep="") )  
+
+### --- Load Pmisc and refresh the package
+library(Pmisc)
+RefreshPackage(package.name=package.name,commit.message=commit.message)
+
+do.not.run <- FALSE
+if (do.not.run) {
+  library(devtools)
+  install_github(username="pjnewcombe", repo="R2BGLiMS")
+}
---
+++ RERUN_R2BGLiMS_EXAMPLES.R
@@ -0,0 +1,33 @@
+library(R2BGLiMS)
+
+### --- Logistic regression with two model components
+utils::data(biopsy, package = "MASS")
+for (v in paste("V",c(1:9),sep="")) {
+  biopsy[,v] <- (biopsy[,v]-mean(biopsy[,v],na.rm=T))/sd(biopsy[,v],na.rm=T)
+}
+biopsyResults <- R2BGLiMS(
+  likelihood="Logistic", data=biopsy, outcome.var="class",
+  model.space.priors=list(
+    list("Rate"=0.5, "Variables"=paste("V",c(1:3),sep="")),
+    list("Rate"=0.1, "Variables"=paste("V",c(4:9),sep=""))
+  )
+)
+save(biopsyResults,
+     file="~/Dropbox/Work Projects/R Packages/R2BGLiMS/data/biopsyResults.rda")
+
+### --- Survival analysis with priors provided
+utils::data(VA, package = "MASS")
+predictors <- c("treat","age","Karn","diag.time","cell","prior")
+beta.priors <- data.frame(
+  cbind(rep(0,length(predictors)),rep(10,length(predictors))),
+  row.names=predictors)
+VAResults <- R2BGLiMS(
+  likelihood="Weibull",
+  data=VA,
+  outcome.var="status",
+  times.var="stime",
+  model.space.priors=list(list("a"=1,"b"=1,"Variables"=predictors)),
+  beta.prior=beta.priors
+)
+save(VAResults,
+     file="~/Dropbox/Work Projects/R Packages/R2BGLiMS/data/VAResults.rda")
