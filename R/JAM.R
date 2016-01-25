7def78dfa7a1bb59e423a231bc38e56d1fe5c071
Paul Newcombe <pjnewcombe@gmail.com>
2016-01-25 12:00:25 +0000

    Added option to JAM to perform full MCMC sampling.

--- R/JAM.R
+++ R/JAM.R
@@ -7,6 +7,10 @@ NULL
 #' @title JAM (Joint Analysis of Marginal statistics)
 #' @name JAM
 #' @inheritParams R2BGLiMS
+#' @param full.mcmc.sampling By default JAM only samples models, since the parameters can be analytically integrated out.
+#' If you would like to force JAM to perform full Reversible Jump MCMC of models and parameters (effects and residual), then
+#' set this to TRUE. The posterior summaries can be seen using \code{\link{PrettyResultsTable}}. Note that this option is
+#' forced to false if inference via model enumeration is requested by setting enumerate.up.to.dim>0.
 #' @param n.cases If the marginal.betas contain log-Odds Ratios, please specify the number of cases
 #' with this option, so that JAM can calculate the case proportion in order to invoke an approximate 
 #' transformation between the linear and logistic scales.
@@ -35,9 +39,11 @@ JAM <- function(
   g.prior=TRUE,
   tau=NULL,
   enumerate.up.to.dim=0,
+  full.mcmc.sampling=FALSE,
   n.mil=1,
   seed=1,
-  n.cases=NULL
+  n.cases=NULL,
+  extra.arguments=NULL
 ) {
   
   #######################################
@@ -49,20 +55,34 @@ JAM <- function(
     tau <- max(n, length(marginal.betas)^2)
   }
   
+  #####################################################
+  ### --- Whether to perform full MCMC sampling --- ###
+  #####################################################
+  
+  if (full.mcmc.sampling) {
+    which.blgims.jam.method <- "JAM_MCMC"
+  } else {
+    which.blgims.jam.method <- "JAM"    
+  }
+  
   #######################################
   ### --- Logistic transformation --- ###
   #######################################
   
-  extra.arguments <- NULL
   if (!is.null(n.cases)) {
     cat("\nLog-Odds Ratios were provided.\n")
     cat("\nInvoking the logistic -> linear transformation.")
     phi <- n.cases/n
     marginal.betas <- marginal.betas*phi*(1-phi)
-    extra.arguments=list(
-      "GaussianResidualVarianceInvGammaPrior_a" = n,
-      "GaussianResidualVarianceInvGammaPrior_b" = (n-1)*phi*(1-phi)
-    )
+    if (is.null(extra.arguments)) {
+      extra.arguments=list(
+        "GaussianResidualVarianceInvGammaPrior_a" = n,
+        "GaussianResidualVarianceInvGammaPrior_b" = (n-1)*phi*(1-phi)
+      )      
+    } else {
+      extra.arguments[["GaussianResidualVarianceInvGammaPrior_a"]] = n
+      extra.arguments[["GaussianResidualVarianceInvGammaPrior_b"]] = (n-1)*phi*(1-phi)
+    }
   }
   
   ############################
@@ -70,6 +90,7 @@ JAM <- function(
   ############################
   
   if (enumerate.up.to.dim > 0) {
+    which.blgims.jam.method <- "JAM" # Full MCMC sampling is disabled for model enumeration
     ### --- Enumeration
     n.mil <- 0.000001 # Set to minimum number of iterations
     if (!is.list(X.ref)|length(X.ref)==1) {
@@ -77,7 +98,7 @@ JAM <- function(
       ### --- Enumeration - 1 region --- ###
       ######################################
       results <- R2BGLiMS(
-        likelihood="JAM",
+        likelihood=which.blgims.jam.method,
         marginal.betas=marginal.betas,
         n=n,
         X.ref=X.ref,
@@ -103,7 +124,7 @@ JAM <- function(
         vars.ld.block <- colnames(X.ref[[ld.block]])
         model.space.priors.ld.block$Variables <- vars.ld.block
         results.r2bglims.g <- R2BGLiMS(
-          likelihood="JAM",
+          likelihood=which.blgims.jam.method,
           marginal.betas=marginal.betas[vars.ld.block],
           n=n,
           X.ref=X.ref[[ld.block]],
@@ -133,7 +154,7 @@ JAM <- function(
   } else {
     ### --- RJMCMC    
     results <- R2BGLiMS(
-      likelihood="JAM",
+      likelihood=which.blgims.jam.method,
       marginal.betas=marginal.betas,
       n=n,
       X.ref=X.ref,
