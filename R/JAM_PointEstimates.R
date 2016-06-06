b526988cdc35ce862b90bd86b8457d4c6d9d16ec
Paul Newcombe <pjnewcombe@gmail.com>
2016-06-06 17:05:28 +0100

    JAM_PointEstimates fix.

--- R/JAM_PointEstimates.R
+++ R/JAM_PointEstimates.R
@@ -17,28 +17,28 @@ JAM_PointEstimates <- function(
 ) {
   
   # Mean centre the columns of X, to avoid inferring the intercept
-  for (v in 1:ncol(X)) {
-    X[,v] <- X[,v] - mean(X[,v])
+  for (v in 1:ncol(X.ref)) {
+    X.ref[,v] <- X.ref[,v] - mean(X.ref[,v])
   }
-  n <- nrow(X) # N is arbitrary for the point estimator, use the size of the reference matrix.
+  n <- nrow(X.ref) # N is arbitrary for the point estimator, use the size of the reference matrix.
   
   # Construct the z_jam outcome vector. The element for each SNP is constructed from:
   # 1) Mean y-values within each of the three genotype groups - inferred from marginal effect
   # 2) Count of each genotype - inferred from reference data
   z_jam <- NULL
-  for (v in 1:length(lmBetaHat)) {
-    # Genotype group counts according to proportions in reference X
-    n1 <- n * table(X[,v])[2]/sum(table(X[,v]))
-    n2 <- n * table(X[,v])[3]/sum(table(X[,v]))
+  for (v in 1:length(marginal.betas)) {
+    # Genotype group counts according to proportions in reference X.ref
+    n1 <- n * table(X.ref[,v])[2]/sum(table(X.ref[,v]))
+    n2 <- n * table(X.ref[,v])[3]/sum(table(X.ref[,v]))
     # Genotype group y-means (for a mean-centred y)
-    y0 <- -(n1 * lmBetaHat[v] + n2 * 2 *lmBetaHat[v])/n
-    y1 <- y0 + lmBetaHat[v]
-    y2 <- y0 + 2*lmBetaHat[v]
-    z_jam <- c(z_jam, y1 * n1 + 2 * y2 * n2) # t(X)%*%y  
+    y0 <- -(n1 * marginal.betas[v] + n2 * 2 *marginal.betas[v])/n
+    y1 <- y0 + marginal.betas[v]
+    y2 <- y0 + 2*marginal.betas[v]
+    z_jam <- c(z_jam, y1 * n1 + 2 * y2 * n2) # t(X.ref)%*%y  
   }
   
   # Take Cholesky decomposition and construct estimate. Formula is just below eq (13) in paper.
-  L <- chol(t(X) %*% X)
+  L <- chol(t(X.ref) %*% X.ref)
   z_L <- solve(t(L)) %*% z_jam
   multivariate.beta.hat <- t(solve(t(L) %*% L) %*% t(L) %*% z_L)  
   
